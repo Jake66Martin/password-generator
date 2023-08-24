0a258536575513e64d410b7e87577bcd2362ae4b
leviathan <jake66martin@hotmail.com>
2023-08-23 21:26:03 -0400

    added alert/prompt/return code for password

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  alert("Beep boop! Bzzzzzz. You have activated password bot 3000. It seems you need help with a new password.")
+  
+  prompt("Bzzzz bzzzz *machine noises* My systems tell me that you have a choice between 8 and 128 characters for your password. What will it be? ")
+  
+  prompt("")
+
+
+  let password = Math.random()*101;
+
+  return password;
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
